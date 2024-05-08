6cd538ddd951d11886b8c75dd72f1d480a300196
Claudemar Leal <89711265+clauderleal@users.noreply.github.com>
2024-05-09 00:47:52 +0100

    add test

--- backend/test/models/article_test.rb
+++ backend/test/models/article_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "article is not valid without a title" do
+    article = Article.new(body: "Lorem ipsum", published: true)
+    assert_not article.valid?
+  end
+
+  test "article is not valid without a body" do
+    article = Article.new(title: "Test Article", published: true)
+    assert_not article.valid?
+  end
+
+  test "article is valid with all attributes" do
+    article = Article.new(title: "Test Article", body: "Lorem ipsum", published: true)
+    assert article.valid?
+  end
+
+  test "article default published value" do
+    article = Article.new(title: "Test Article", body: "Lorem ipsum")
+    assert_not article.published?
+  end
 end
